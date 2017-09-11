af21777a9fd94f39007096c9883b9ba957821587
Milos <zbiljicmilos@gmail.com>
2017-09-11 20:08:44 +0200

    Relations

--- app/Comment.php
+++ app/Comment.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Gallery;
 
 class Comment extends Model
 {
-    //
+    public function gallery()
+    {
+    	return $this->belongsTo(Gallery::class);
+    }
 }
--- app/Gallery.php
+++ app/Gallery.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Image;
+use App\Comment;
 
 class Gallery extends Model
 {
-    //
+    public function images() 
+    {
+    	return $this->hasMany(Image::class);
+    }
+
+    public function comments()
+    {
+    	return $this->hasMany(Comment::class);
+    }
 }
