b2761cc798832b5afba8b3ea4ec565636a0a4c8a
Milos <zbiljicmilos@gmail.com>
2017-09-14 01:38:05 +0200

    Routes

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,9 @@ Route::post('register', 'ApiAuth\RegisterController@register');
 
 Route::get('galleries', 'GalleryController@index');
 Route::post('galleries', 'GalleryController@store');
+Route::post('galleries/{id}', 'GalleryController@update');
+Route::get('galleries/{id}', 'GalleryController@show');
+Route::delete('galleries/{id}', 'GalleryController@destroy');
+
+Route::post('comments', 'CommentController@store');
+Route::delete('comments/{id}', 'CommentController@destroy');
